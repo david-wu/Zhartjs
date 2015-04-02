f76135db47ffc77c45563b14b20379c39c4cd157
David Wu <david.wuu@gmail.com>
2015-04-01 22:52:58 -0700

    Added layers, xaxis, yaxis

--- src/Zhart.Core.js
+++ src/Zhart.Core.js
@@ -8,18 +8,45 @@
  * @param {options}
  */
 root.Zhart = function Zhart (context, options) {
-    if (!options) { options = {} };
-    this.width = options.width
+
+    options = options || {};
+
+    var svgWidth = options.width
         || document.defaultView.getComputedStyle(context).getPropertyValue('Width');
-    this.height = options.height
+
+    var svgHeight = options.height
         || document.defaultView.getComputedStyle(context).getPropertyValue('Height');
-    this.aspectRatio = this.width / this.height;
 
     this.svg = d3.select(context)
         .append('svg')
         .classed('zhart', true)
+        .attr('width', svgWidth)
+        .attr('height', svgHeight)
+
+    // Margins seperate the labels, and axis from the center of the graph
+    this.margins = options.margins || {
+        top: 10,
+        left: 30,
+        bottom: 30,
+        right:10
+    };
+
+    // Contains most graphics, leaving margin room
+    this.width = svgWidth-this.margins.left-this.margins.right;
+    this.height = svgHeight-this.margins.top-this.margins.bottom;
+    this.vis = this.svg.append('g')
+        .classed('vis', true)
         .attr('width', this.width)
-        .attr('height', this.height);
+        .attr('height', this.height)
+        .attr('transform', 'translate('+this.margins.left+','+this.margins.top+')');
+
+    // Scales used to draw within vis
+    this.yScale = d3.scale.linear()
+        .range([0, this.height]);
+
+    this.xScale = d3.time.scale()
+        .range([0, this.width]);
+
     return this;
 }
 
--- src/Zhart.Microchart.js
+++ src/Zhart.Microchart.js
@@ -4,21 +4,45 @@
 var Zhart = root.Zhart
     , helpers = Zhart.helpers;
 
-var features = [Microchart, test];
+var features = [backGround, text];
+var layers = [xAxis, yAxis, line];
 
 Zhart.prototype.Microchart = function (datasets, options) {
     if (!options) {options = {}};
     for (var i = 0; i<features.length; i++) {
         features[i](this, datasets, options);
     };
+
+    this.redraw = function(){
+        // TODO: Add autoScaling
+        this.xScale.domain([0,10])
+        this.yScale.domain([0,10])
+        for(var i = 0; i < layers.length; i++){
+            layers[i](this, datasets, options)
+        }
+    }
+
+    // TODO: remove performance test
+    var t = Date.now()
+    for(var i = 0; i < 1000; i++){
+        this.redraw();        
+    }
+    console.log('1000 redraws took: ', Date.now() - t, 'ms');
+
 }
 
-function Microchart (zhart, datasets, options) {
+}(this));
+
+/*
+    TODO: Move this section out to different files
+*/
+
+function backGround (zhart, datasets, options) {
     var bgColor = options.bgColor || 'black';
     zhart.svg.style('background-color', bgColor);
 }
 
-function test (zhart, datasets, options) {
+function text (zhart, datasets, options) {
     var text = options.text || 'what';
     zhart.svg.append('text')
         .attr('x', zhart.width/2)
@@ -26,4 +50,40 @@ function test (zhart, datasets, options) {
         .text(text);
 }
 
-}(this));
+
+// A layer that contains the xAxis
+function xAxis(zhart){
+    var xAxis = d3.svg.axis()
+        .scale(zhart.xScale)
+        .orient('bottom')
+        .ticks(3)
+        .tickSize(1)
+    var xAxisSvg = zhart.vis.selectAll('g.x-axis')
+        .data([1])
+        .enter()
+        .append('g')
+            .attr('class', 'x-axis')
+            .attr("transform", "translate(0," + zhart.height + ")")
+            .call(xAxis);
+}
+
+
+// A layer that contains the yAxis
+function yAxis(zhart){
+    var yAxis = d3.svg.axis()
+        .scale(zhart.yScale)
+        .orient('left')
+        .ticks(3)
+        .tickSize(1)
+    var yAxisSvg = zhart.vis.selectAll('g.y-axis')
+        .data([1])
+        .enter()
+        .append('g')
+            .attr('class', 'y-axis')
+            .call(yAxis);
+}
+
+// A layer that contains lines for linegraph
+function line(zhart){
+
+}
