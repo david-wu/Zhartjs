6bea4e24833f2f7511366183980284691960113e
David Wu <david.wuu@gmail.com>
2015-04-02 21:16:34 -0700

    Seperated out Layers, convenience function

---
+++ src/Layers.helpers.js
@@ -0,0 +1,108 @@
+(function(root){
+
+var Zhart = root.Zhart;
+
+/*
+	layers lets you retrieve the corresponding layer functions
+
+	Zhart.layers('xAxis', 'yAxis', 'line')
+		=> [func, func, func]
+	Zhart.layers['xAxis']
+		=> func
+*/
+
+Zhart.layers = function(){
+	return _.map(arguments, function(layer){
+		return Zhart.layers[layer];
+	})
+};
+var layers = Zhart.layers;
+
+// A layer that draws the xAxis
+layers.xAxis = function xAxis(zhart){
+    var xAxis = d3.svg.axis()
+        .scale(zhart.xScale)
+        .orient('bottom')
+        .ticks(3)
+        .tickSize(1);
+    var xAxisSvg = zhart.vis.selectAll('g.x-axis')
+        .data([1])
+        .enter()
+        .append('g')
+            .attr('class', 'x-axis')
+            .attr("transform", "translate(0," + zhart.height + ")")
+            .call(xAxis);
+};
+
+// A layer that draws the yAxis
+layers.yAxis = function yAxis(zhart){
+    var yAxis = d3.svg.axis()
+        .scale(zhart.yScale)
+        .orient('left')
+        .ticks(3)
+        .tickSize(1);
+    var yAxisSvg = zhart.vis.selectAll('g.y-axis')
+        .data([1])
+        .enter()
+        .append('g')
+            .attr('class', 'y-axis')
+            .call(yAxis);
+};
+
+// A layer that draws lines for linegraph
+layers.line = function line(zhart, datasets){
+
+    // Accepts data and returns a line path
+    var lineFunc = d3.svg.line()
+        .x(function(d){return zhart.xScale(d[0]);})
+        .y(function(d){return zhart.yScale(d[1]);});
+
+    // Accepts data and generates a class name
+    var classFunc = function(d, index){
+        return 'line '+index;
+    }
+
+    // Draws paths using datasets
+    zhart.vis.selectAll('path.line')
+        .data(datasets)
+        .enter()
+        .append('svg:path')
+            .attr('d', lineFunc)
+            .attr('class', classFunc)
+            // TODO: Improve classFunc and put these things in scss
+            .style('fill', 'none')
+            .attr('stroke', 'black')
+            .attr('stroke-width', 1);
+};
+
+// A function that draws an area for linegraph
+layers.area = function area(zhart, datasets){
+
+    // Accepts data and returns a line path
+    var areaFunc = d3.svg.area()
+        .x(function(d){return zhart.xScale(d[0]);})
+        .y0(function(){return zhart.yScale(0);})
+        .y1(function(d){return zhart.yScale(d[1]);});
+
+    // Accepts data and generates a class name
+    var classFunc = function(d, index){
+        return 'area '+index;
+    }
+
+    // Draws paths using datasets
+    zhart.vis.selectAll('path.area')
+        .data(datasets)
+        .enter()
+        .append('svg:path')
+            .attr('d', areaFunc)
+            .attr('class', classFunc)
+            // TODO: Improve classFunc and put these things in scss
+            .style('fill', 'red')
+            .style('opacity', 0.5)
+            .attr('stroke', 'none')
+            .attr('stroke-width', 1)
+            .on('mouseover', function(){
+                console.log('ZHART')
+            });
+};	
+})(this);
--- src/Zhart.Microchart.js
+++ src/Zhart.Microchart.js
@@ -2,10 +2,11 @@
 'use strict';
 
 var Zhart = root.Zhart
-    , helpers = Zhart.helpers;
+var helpers = Zhart.helpers;
 
-var features = [backGround, text];
-var layers = [xAxis, yAxis, line, area];
+var features = [background, text];
+
+var layers = Zhart.layers('xAxis', 'yAxis', 'line', 'area');
 
 Zhart.prototype.Microchart = function (datasets, options) {
     if (!options) {options = {}};
@@ -33,13 +34,12 @@ Zhart.prototype.Microchart = function (datasets, options) {
     console.log('1000 redraws took: ', Date.now() - t, 'ms');
 }
 
-}(this));
 
 /*
     TODO: Move this section out to different files
 */
 
-function backGround (zhart, datasets, options) {
+function background (zhart, datasets, options) {
     var bgColor = options.bgColor || 'black';
     zhart.svg
         .style('background-color', bgColor);
@@ -53,87 +53,4 @@ function text (zhart, datasets, options) {
         .text(text);
 }
 
-// A layer that draws the xAxis
-function xAxis(zhart){
-    var xAxis = d3.svg.axis()
-        .scale(zhart.xScale)
-        .orient('bottom')
-        .ticks(3)
-        .tickSize(1);
-    var xAxisSvg = zhart.vis.selectAll('g.x-axis')
-        .data([1])
-        .enter()
-        .append('g')
-            .attr('class', 'x-axis')
-            .attr("transform", "translate(0," + zhart.height + ")")
-            .call(xAxis);
-}
-
-// A layer that draws the yAxis
-function yAxis(zhart){
-    var yAxis = d3.svg.axis()
-        .scale(zhart.yScale)
-        .orient('left')
-        .ticks(3)
-        .tickSize(1);
-    var yAxisSvg = zhart.vis.selectAll('g.y-axis')
-        .data([1])
-        .enter()
-        .append('g')
-            .attr('class', 'y-axis')
-            .call(yAxis);
-}
-
-// A layer that draws lines for linegraph
-function line(zhart, datasets){
-
-    // Accepts data and returns a line path
-    var lineFunc = d3.svg.line()
-        .x(function(d){return zhart.xScale(d[0]);})
-        .y(function(d){return zhart.yScale(d[1]);});
-
-    // Accepts data and generates a class name
-    var classFunc = function(d, index){
-        return 'line '+index;
-    }
-
-    // Draws paths using datasets
-    zhart.vis.selectAll('path.line')
-        .data(datasets)
-        .enter()
-        .append('svg:path')
-            .attr('d', lineFunc)
-            .attr('class', classFunc)
-            // TODO: Improve classFunc and put these things in scss
-            .style('fill', 'none')
-            .attr('stroke', 'black')
-            .attr('stroke-width', 1);
-}
-
-// A function that draws an area for linegraph
-function area(zhart, datasets){
-
-    // Accepts data and returns a line path
-    var areaFunc = d3.svg.area()
-        .x(function(d){return zhart.xScale(d[0]);})
-        .y0(function(){return zhart.yScale(0);})
-        .y1(function(d){return zhart.yScale(d[1]);});
-
-    // Accepts data and generates a class name
-    var classFunc = function(d, index){
-        return 'area '+index;
-    }
-
-    // Draws paths using datasets
-    zhart.vis.selectAll('path.area')
-        .data(datasets)
-        .enter()
-        .append('svg:path')
-            .attr('d', areaFunc)
-            .attr('class', classFunc)
-            // TODO: Improve classFunc and put these things in scss
-            .style('fill', 'red')
-            .style('opacity', 0.5)
-            .attr('stroke', 'none')
-            .attr('stroke-width', 1);
-}
+}(this));
